a4a1f095ad34def43671056e091926a3c0201789
hoangdiep271024 <144908726+hoangdiep271024@users.noreply.github.com>
2024-12-17 12:41:05 +0700

    diep

--- src/Admin/Dashboard.jsx
+++ src/Admin/Dashboard.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 export default function Dashboard() {
   console.log('1')
   return (
-    <div style= {{textAlign: 'center'}}>Dashboard</div>
+    <div style= {{textAlign: 'start'}}>Dashboard</div>
   )
 }
